a1ddd355a9f437ce66cef71f69cdfb2845602c43
blitz-flex <abashidzeikaa@gmail.com>
2025-12-13 13:05:40 +0400

    CV Builder welcome page

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Welcome from './components/Welcome'
+import './styles/App.css'
+
+function App() {
+  return <Welcome />
+}
+
+export default App
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import '../styles/Welcome.css'
+
+function Welcome() {
+  return (
+    <div className="welcome">
+      <h1>CV Builder</h1>
+    </div>
+  )
+}
+
+export default Welcome
